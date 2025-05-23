4cca43b7d303b93db2e0a95ec73df2878b95f62e
G Manjunth <manjudba1222@gmail.com>
2025-05-23 14:24:45 +0530

    hi

--- database/backend.sql
+++ database/backend.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE IF NOT EXISTS transactions;
-USE transactions;
-
-CREATE TABLE IF NOT EXISTS transactions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    amount INT,
-    description VARCHAR(255)
-);
 
 CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
 GRANT ALL PRIVILEGES ON *.* TO 'expense'@'%';
